97ac560cf285c0a414b831c7c9b22c43abe344e5
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-05-01 14:37:22 +0300

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyACKfBsdylbRPNSgqeAkjwVpfDuwJrdwck",
+    authDomain: "chatty-ba4a6.firebaseapp.com",
+    projectId: "chatty-ba4a6",
+    storageBucket: "chatty-ba4a6.appspot.com",
+    messagingSenderId: "959862813342",
+    appId: "1:959862813342:web:ae39af377ad9db900dc18f",
+    measurementId: "G-02V6N1YFWH"
+  };
